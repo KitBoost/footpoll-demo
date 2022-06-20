4bb321d60f5f2c0733b1ef994743fa8f4fc92cdc
Joe Sandmeyer <github.com@unconscious.com>
2022-06-20 16:40:20 -0400

    published tally

--- plugins/pollmenu/index.js
+++ plugins/pollmenu/index.js
@@ -63,11 +63,10 @@ class VoterStatus {
 class PollStatus {
 
   constructor(inChoices, inCatchAllKey = null){
-    this.choices          = inChoices;
-    this.catchAllKey      = inCatchAllKey;
-    this.isUsingCatchAll  = isValidPollChoiceName(inCatchAllKey);
-    //this.myUserID         = inMyUserID;
-    this.tally            = new Object;
+    this.catchAllKey          = inCatchAllKey;
+    this.isUsingCatchAll      = isValidPollChoiceName(inCatchAllKey);
+    this.tally                = new Object;
+    this.recentPublishedTally = null;
 
     let needToAddCatchAll = this.isUsingCatchAll;
     
@@ -90,22 +89,42 @@ class PollStatus {
   }// constructor()
   
   resetForNextPoll(){
-    this.pollsterUserID      = '';
-    this.amIPollster         = false;
-    this.isPollInProgress    = false;
-    this.isMyBallotSubmitted = false;
-    //this.tally               = {};
+    this.pollsterUserID      = null;
+    //
+    //this.isTallyInProgress   = false;
+    //this.isPollRequested     = false;
+    //this.didIRequestPoll     = false;
+    //
+    //this.amIPollster         = false;
+    //this.isMyBallotSubmitted = false;
     
     for (let key in this.tally) {
       this.tally[key] = 0;
     }
 
-    //this.ctPollDiamonds      = 0;
-    //this.ctPollHearts        = 0;
-    //this.ctPollSpades        = 0;
-    //this.ctPollAbstentions   = 0;
     console.log('eo Pollstatus.resetForNextPoll');
     console.dir(this);
+  }
+  
+  isPollInProgress(){
+    return !! this.pollsterUserID;
+  }
+  
+  hearAboutNewPoll(inPollsterUserID){
+    if (! this.pollsterUserID || (inPollsterUserID < this.pollsterUserID)){
+      // I've either not yet logged a pollster or new one outranks. 
+      this.pollsterUserID = inPollsterUserID;
+    }
+  }// hearAboutNewPoll
+  
+  hearAboutCleanup(){
+    this.resetForNextPoll();
+  }// hearAboutCleanup
+  
+  amIThePollster(inMyUserID) {
+    console.log('eo Pollstatus.amIThePollster');
+    console.dir(this);
+    return (inMyUserID == this.pollsterUserID);
   }
   
   get catchAllKey(){
@@ -147,7 +166,7 @@ class PollStatus {
   getCountForSpecificChoice(inChoice){
     let foundKeyMatch = false;
     let retVal = 0;
-      
+    //  
     for (let key in this.tally) {
       if (inChoice == key) {
         retVal = this.tally[key];
@@ -155,101 +174,77 @@ class PollStatus {
         break;
       }
     }
-    
-    if (! foundKeyMatch && this.isUsingCatchAll){
-      retVal = this.tally[this.catchAllKey];
+    //
+    // Return catchall count only when isUsingCatchAll and exact match to catchall key.
+    if (! foundKeyMatch){
+      throw 'Invalid choice.';
     }
-    
+    //
     return retVal;
   }// getCountForSpecificChoice()
 
-  /* --- *
-  incrementDiamondsTally(){
-    this.ctPollClubs++;
+  getCountForCatchall(){
+    if (this.isUsingCatchAll){
+      return getCountForSpecificChoice(catchAllKey);
+    }
+    return 0;
+  }// getCountForSpecificChoice()
+
+  getCountForPublishedChoice(inChoice){
+    let foundKeyMatch = false;
+    let retVal = 0;
+    //  
+    for (let key in this.recentPublishedTally) {
+      if (inChoice == key) {
+        retVal = this.recentPublishedTally[key];
+        foundKeyMatch = true;
+        break;
+      }
+    }
+    //
+    // Return catchall count only when isUsingCatchAll and exact match to catchall key.
+    if (! foundKeyMatch){
+      throw 'Invalid choice.';
+    }
+    //
+    return retVal;
+  }// getCountForPublishedChoice()
+
+  importPublshedTally_FromJsonString(inTallyJsonString) {
+    this.recentPublishedTally = JSON.parse(inTallyJsonString);
   }
-  
-  getDiamondsTally(){
+
+  exportTally_AsJsonString() {
+    return JSON.stringify(this.tally);
   }
-  
-  incrementHeartsTally(){
-    this.ctPollClubs++;
-  }
-  
-  getHeartsTally(){
-  }
-  
-  incrementSpadesTally(){
-    this.ctPollClubs++;
-  }
-  
-  getSpadesTally(){
-  }
-  
-  incrementAbstentionsTally(){
-  }
-  
-  getAbstentionsTally(){
-  }
-  /* --- */
-  
 }// class PollStatus
 
 
 module.exports = class PollMenuPlugin extends BasePlugin {
 
-  /** Plugin info */
+  // Plugin info
   static get id()             { return 'pollmenu-plugin' };
   static get name()           { return 'Poll Menu' };
   static get description()    { return 'Presents a menu to query poll results.' };
   
+  // Client instance properties
   padList       = [];   // List of pads that trigger a poll choice when near.
-  //myUserID      = '';
-  myVoterStatus = null; //new VoterStatus();
-  myPollStatus  = null; //new PollStatus();
+  myVoterStatus = null; // My status as a voter.
+  myPollStatus  = null; // Status of current or recent poll.
   
-  // Status of current or recent poll.
-  /* --- *
-  pollsterUserID      = '';
-  amIPollster         = false;
-  isPollInProgress    = false;
-  isMyBallotSubmitted = false;
-  ctPollClubs         = 0;
-  ctPollDiamonds      = 0;
-  ctPollHearts        = 0;
-  ctPollSpades        = 0;
-  ctPollAbstentions   = 0;
-  
-  pollStatus = {
-    pollsterUserID      : '',
-    amIPollster         : false;
-    isPollInProgress    : false;
-    isMyBallotSubmitted : false;
-    ctPollClubs         = 0;
-    ctPollDiamonds      = 0;
-    ctPollHearts        = 0;
-    ctPollSpades        = 0;
-    ctPollAbstentions   = 0;  
-  }
-  /* --- */
-
-  /** Called on load */
   onLoad() {
-
     this.myPollStatus = new PollStatus(
       ['clubs', 'diamonds', 'hearts', 'spades'], 'abstentions');
-
+    //
     // Who am I?
-    /* --- */
     this.user.getID().then(inUserID => {
-      //this.myUserID = inUserID;
       this.myVoterStatus = new VoterStatus(inUserID);
       console.log(`I am ${this.myVoterStatus.userID}`);
     }).catch(err => {
       console.warn('Error fetching this.user.getID() in onLoad() -- ', err)
     })
-    /* --- */
-
-    // Allow message to be configured
+    //
+    // Prepare a HUD display.
     this.menus.register({
       id: 'hud',
       title: 'hudmenu-label',
@@ -260,60 +255,60 @@ module.exports = class PollMenuPlugin extends BasePlugin {
           width: 500,
           height: 500
       }
-    })
-
-    // Create a button in the toolbar
+    });
+    //
+    // Create buttons in the toolbar.
     this.menus.register({
       id: 'menu-poll',
       icon: this.paths.absolute('pollmenu-btn.png'),
       text: 'Poll',
+      adminOnly: true,
       action: () => this.onBtnTriggerPoll(),
       section: 'controls'
-    })
-
+    });
     this.menus.register({
       id: 'menu-clubs',
       icon: this.paths.absolute('BtnClubs.png'),
       text: 'Clubs',
       action: () => this.onBtnClubs(),
-    })
-
+    });
     this.menus.register({
       id: 'menu-diamonds',
       icon: this.paths.absolute('BtnDiamonds.png'),
       text: 'Diamonds',
       action: () => this.onBtnDiamonds(),
-    })
-
+    });
     this.menus.register({
       id: 'menu-hearts',
       icon: this.paths.absolute('BtnHearts.png'),
       text: 'Hearts',
       action: () => this.onBtnHearts(),
-    })
-
+    });
     this.menus.register({
       id: 'menu-spades',
       icon: this.paths.absolute('BtnSpades.png'),
       text: 'Spades',
       action: () => this.onBtnSpades(),
+    });
+    this.menus.register({
+      id: 'menu-nudge',
+      icon: this.paths.absolute('pollmenu-btn.png'),
+      text: 'Nudge',
+      adminOnly: true,
+      action: () => this.onBtnTriggerNudge(),
+      section: 'controls'
     })
-
   }// onLoad()
   
   onMessage(data) {
-    console.log(`I am ${this.myVoterStatus.userID} hearing ${data.action}`);
+    console.log(`I am ${this.myVoterStatus.userID} hearing message ${JSON.stringify(data)}`);
     //
     if (data.action === 'announcing-poll') {
+      this.myPollStatus.hearAboutNewPoll(data.pollster);
       const toastID = this.menus.toast({
         icon: this.paths.absolute('pollmenu-btn.png'),
         text: '5 seconds to vote with your feet.',
         textColor: '#2DCA8C',
-        // buttonColor: '#FFFFFF',
-        // buttonText: ' ', // 'Enable'
-        // buttonAction: () => { console.log('clicked ack') },
-        // buttonCancelText: ' ', // 'Disable'
-        // buttonCancelAction: () => { console.log('clicked cancel') },
         duration: 5000
       });
       // console.log(`toastID: ${toastID}`); // toastID is a promise
@@ -325,47 +320,88 @@ module.exports = class PollMenuPlugin extends BasePlugin {
         console.warn('Error fetching user choice -- onMessage requesting-ballots', err)
       });
     } else if (data.action === 'sending-ballot') {
+      // This message was addressed directly to a pollster.
       console.log(`onMessage increment choice ${data.choice}`);
       this.myPollStatus.incrementCountForSpecificChoice(data.choice);
+    } else if (data.action === 'publishing-results') {
+      console.log(`onMessage publishing-results`);
+      this.onResultsReceived(data.tally);
+    } else if (data.action === 'cleanup-after-poll') {
+      console.log(`onMessage cleanup`);
+      this.myPollStatus.hearAboutCleanup();
     } else {
       console.log(`onMessage unrecognized action ${data.action}`);
     }
   }// onMessage
 
-  pollsterRequestBallots(thisPollster) {
-    console.log(`I am ${thisPollster.myVoterStatus.userID} in pollsterRequestBallots()`);
+  pollsterRequestBallots(thisInstance) {
+    // thisInstance param is a workaround for 'this' context getting lost from setTimeout() callback.
+    console.log(`I am ${thisInstance.myVoterStatus.userID} in pollsterRequestBallots()`);
     //
-    thisPollster.messages.send({ action:'requesting-ballots', pollster: thisPollster.myVoterStatus.userID }, true);
-    /* --- *
-    thisPollster.user.getID().then(inUserID => {
-      console.log(`send requesting-ballots by ${inUserID}`);
-      thisPollster.messages.send({ action:'requesting-ballots', pollster: inUserID }, true);
-    }).catch(err => {
-      console.warn('Error fetching cur user ID -- pollsterRequestBallots -- ', err);
-    })
-    /* --- */
+    thisInstance.messages.send({ action:'requesting-ballots', pollster: thisInstance.myVoterStatus.userID }, true);
   }// onRequestPollResults
   
-  pollsterTally(thisPollster) {
-    console.log(`begin pollsterTally()`);  
+  pollsterTally(thisInstance) {
+    // thisInstance param is a workaround for 'this' context getting lost from setTimeout() callback.
+    console.log(`begin pollsterTally() thisInstance obj follows:`);
+    console.dir(thisInstance);
+    
+    let amIThePollster = thisInstance.myPollStatus.amIThePollster( thisInstance.myVoterStatus.userID );
+    
+    console.log(`step pollsterTally() amIThePollster ${amIThePollster}`);
+    if (! amIThePollster) return;
+    
+    thisInstance.messages.send({ action:'publishing-results', tally: thisInstance.myPollStatus.exportTally_AsJsonString() }, true);
   
-    const ctClubs     = thisPollster.myPollStatus.getCountForSpecificChoice('clubs');
-    const ctDiamonds  = thisPollster.myPollStatus.getCountForSpecificChoice('diamonds');
-    const ctHearts    = thisPollster.myPollStatus.getCountForSpecificChoice('hearts');
-    const ctSpades    = thisPollster.myPollStatus.getCountForSpecificChoice('spades');
+    /* --- *
+    const ctClubs     = thisInstance.myPollStatus.getCountForSpecificChoice('clubs');
+    const ctDiamonds  = thisInstance.myPollStatus.getCountForSpecificChoice('diamonds');
+    const ctHearts    = thisInstance.myPollStatus.getCountForSpecificChoice('hearts');
+    const ctSpades    = thisInstance.myPollStatus.getCountForSpecificChoice('spades');
+    
     const strPollResults = "Poll results:<br>"
       + `${ctClubs} \u2663<br>${ctDiamonds} \u2666<br>${ctHearts} \u2665<br>${ctSpades} \u2660`;
     const htmlPollResults = `<div style="color: black; background-color: #ffffff; font-size: 24px; ">${strPollResults}</div>`;
   
-    console.log(`I am ${thisPollster.myVoterStatus.userID} in pollsterTally()`);
+    console.log(`I am ${thisInstance.myVoterStatus.userID} in pollsterTally()`);
 
     console.log(htmlPollResults);
     
-    thisPollster.menus.postMessage({ action: 'hud-set', src: htmlPollResults });
-  }
+    thisInstance.menus.postMessage({ action: 'hud-set', src: htmlPollResults });
+    /* --- */
+  }// pollsterTally()
+  
+  onResultsReceived(inPublishedTally) {
+    console.log(`onResultsReceived by ${this.myVoterStatus.userID}`);
+    console.dir(inPublishedTally);
+    //
+    this.myPollStatus.importPublshedTally_FromJsonString(inPublishedTally);
+  
+    const ctClubs     = this.myPollStatus.getCountForPublishedChoice('clubs');
+    const ctDiamonds  = this.myPollStatus.getCountForPublishedChoice('diamonds');
+    const ctHearts    = this.myPollStatus.getCountForPublishedChoice('hearts');
+    const ctSpades    = this.myPollStatus.getCountForPublishedChoice('spades');
+    
+    const strPollResults = "Poll results:<br>"
+      + `${ctClubs} \u2663<br>${ctDiamonds} \u2666<br>${ctHearts} \u2665<br>${ctSpades} \u2660`;
+    const htmlPollResults = `<div style="color: black; background-color: #ffffff; font-size: 24px; ">${strPollResults}</div>`;
+  
+    console.log(`I am ${this.myVoterStatus.userID} in onResultsReceived()`);
+
+    console.log(htmlPollResults);
+    
+    this.menus.postMessage({ action: 'hud-set', src: htmlPollResults });
+  }// onResultsReceived()
 
   onBtnTriggerPoll() {
-    console.log(`onBtnTriggerPoll by ${this.myVoterStatus.userID}`);      
+    console.log(`onBtnTriggerPoll by ${this.myVoterStatus.userID}`);
+    
+    // Only one poll at a time.
+    if (this.myPollStatus.isPollInProgress()){
+      console.error('Poll request denied. Gotta clean up the old one first.');
+      return;
+    }
+    
     /* --- *
     this.user.getProperty('', 'choice').then(curChoice => {
       console.log(`onBtnTriggerPoll old choice ${curChoice}`);
@@ -377,7 +413,7 @@ module.exports = class PollMenuPlugin extends BasePlugin {
     /* --- */
     
     // T+0 sec: Pollster broadcast 'announcing-poll' so all users can start moving toward a choice.
-    this.messages.send({ action:'announcing-poll' }, true)
+    this.messages.send({ action:'announcing-poll', pollster: this.myVoterStatus.userID }, true)
     
     // T+5 sec: Pollster broadcast 'requesting-ballots' for all to immediately reply to pollster return address.
     const timeoutID_RequestBallots = setTimeout(this.pollsterRequestBallots, 5000, this);
@@ -388,9 +424,16 @@ module.exports = class PollMenuPlugin extends BasePlugin {
     // T+10 sec: Pollster finalizes tally.
     const timeoutID_Tally = setTimeout(this.pollsterTally, 10000, this);
     
+    // 10 < T sec: Cleanup and relinquish.
+    // this.messages.send({ action:'cleanup-after-poll' }, true);  
+    
     //console.log(`timeoutID: ${timeoutID_RequestBallots}`);
     //console.log(`timeoutID: ${timeoutID_Tally}`);
   }// onBtnTriggerPoll
+  
+  onBtnTriggerNudge() {
+    this.myPollStatus.hearAboutCleanup();
+  }
 
   onBtnClubs() {
     this.castVote_OfCurUser('clubs');
